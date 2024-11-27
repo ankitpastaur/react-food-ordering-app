e355450e1c680a1bf86d5c00953a66e36a2bad13
ankitpastor <pastorankit18@gmail.com>
2024-11-27 18:28:45 -0500

    Code refactored

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,29 @@
 import RestaurentCard from "./RestaurentCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 const Body = () => {
-  const [listOfRestaurent, setListOfRestaurent] = useState(resList);
+  const [listOfRestaurent, setListOfRestaurent] = useState([]);
 
-  return (
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+    setListOfRestaurent(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    console.log("setListOfRestaurent calling", setListOfRestaurent);
+  };
+
+  return listOfRestaurent.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="search">Search</div>
       <button
