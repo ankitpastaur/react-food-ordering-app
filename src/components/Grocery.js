80ebbc6d68775a06d0e30047c5cffb14356f0093
ankitpastor <pastorankit18@gmail.com>
2025-01-24 03:32:31 -0500

    Implement lazy loading

---
+++ src/components/Grocery.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Grocery = () => {
+  return (
+    <div>
+      <h1>Grocery Online Store</h1>
+      <p>Welcome to our grocery section!</p>
+    </div>
+  );
+};
+
+export default Grocery;
